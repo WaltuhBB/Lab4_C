314433833f5b16b62975010e0b69c0789219569f
Kirill <salatmail86@gmail.com>
2026-06-07 22:46:08 +0700

    redo_compress_fscanf/fprintf_WIP

--- main.c
+++ main.c
@@ -4,41 +4,16 @@
 #include <stdbool.h>
 
 //Сжать файл
-bool compress(FILE* data, FILE* res)
+bool compress(char* file_in, char* file_out)
 {
     bool check = false;
-    
-    if (data && res)
+
+    if (file_in && file_out)
     {
-        unsigned char buffer[8];
-        int bytes_read = 1;
+        FILE *ptr_in = fopen(file_in, "r");
+        FILE *ptr_out = fopen(file_out, "w");
 
-        unsigned char mask;
-
-        while (bytes_read > 0)
-        {
-            bytes_read = fread(buffer, sizeof(unsigned char), 8, data);
-
-            if (bytes_read == 8)
-            {
-                for (size_t i = 0; i < 7; i++)
-                {
-                    mask = (1 << 6) >> i;
-                    mask = mask & buffer[7];
-                    mask = mask << (i + 1);
-
-                    buffer[i] = buffer[i] | mask;
-                }
-
-                fwrite(buffer, sizeof(unsigned char), 7, res);
-            }
-            else
-            {
-                fwrite(buffer, sizeof(unsigned char), bytes_read, res);
-            }
-        }
-
-        check = true;
+        
     }
 
     return check;
@@ -46,13 +21,7 @@ bool compress(FILE* data, FILE* res)
 
 int main()
 {
-    FILE *ptrIn = fopen("test.txt", "r");
-    FILE *ptrOut = fopen("res.txt", "w");
-
-    compress(ptrIn, ptrOut);
-
-    fclose(ptrIn);    
-    fclose(ptrOut);
+    
 
     return 0;
 }
