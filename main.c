a021392a38cd14fb863a2fb2a80ca320ccd4504f
Kirill <salatmail86@gmail.com>
2026-06-09 23:32:39 +0700

    added_decompress

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int compress(char* file_in, char* file_out)
                 //считываем по 8 байт
                 for (size_t i = 0; i < 8; i++)
                 {
-                    check_scanf = fscanf(ptr_in, "%c", &buffer[i]);
+                    check_scanf = fscanf(ptr_in, "%c", (unsigned char*)&buffer[i]);
                     
                     if (check_scanf == 1)
                     {
@@ -101,12 +101,113 @@ int compress(char* file_in, char* file_out)
     return res;
 }
 
+//распаковать файл
+int decompress(char* file_in, char* file_out)
+{
+    int res = -1;
+
+    if (file_in && file_out)
+    {
+        FILE *ptr_in = fopen(file_in, "rb");
+        FILE *ptr_out = fopen(file_out, "wb");
+
+        if (!ptr_in || !ptr_out)
+        {
+            res = 1;
+        }
+        else
+        {
+            unsigned char buffer[7] = {0};
+            unsigned char recover = 0;
+            
+            size_t bytes_read = 0;
+            size_t bytes_total = 0;
+
+            int check_scanf = 1;
+            int check_printf = 1;
+
+            res = 0;
+
+            while (check_scanf == 1 && res == 0)
+            {
+                bytes_read = 0;
+
+                //считываем по 7 байт
+                for (size_t i = 0; i < 7; i++)
+                {
+                    check_scanf = fscanf(ptr_in, "%c", (unsigned char*)&buffer[i]);
+
+                    if (check_scanf == 1)
+                    {
+                        bytes_read++;
+                        bytes_total++;
+                    }
+                    else
+                    {
+                        i = 7;
+                    }
+                }
+
+                recover = 0;
+
+                //записываем по 8 байт
+                if (res == 0 && bytes_read == 7)
+                {
+                    unsigned char mask = 1 << 6;
+
+                    for (size_t i = 0; i < 7; i++)
+                    {
+                        if (buffer[i] & (1 << 7))
+                        {
+                            recover = recover | mask;
+                        }
+                        
+                        buffer[i] = buffer[i] & (~(1 << 7));
+                        
+                        if (fprintf(ptr_out, "%c", buffer[i]) != 1)
+                        {
+                            res = 3;
+                            i = 7;
+                        }
+
+                        mask = mask >> 1;
+                    }
+
+                    if (fprintf(ptr_out, "%c", recover) != 1)
+                    {
+                        res = 3;
+                    }
+                }
+            }
+
+            //записываем хвост
+            if (res == 0 && bytes_read > 0 && bytes_read < 7)
+            {
+                for (size_t i = 0; i < bytes_read; i++)
+                {
+                    if (fprintf(ptr_out, "%c", buffer[i]) != 1)
+                    {
+                        res = 3;
+                        i = 7;
+                    }
+                }
+            }
+
+            fclose(ptr_in);
+            fclose(ptr_out);
+        }
+    }
+
+    return res;
+}
+
 int main()
 {
     char file_in[20] = "test.txt";
     char file_out[20] = "res.txt";
 
-    compress(file_in, file_out);
+    //compress(file_in, file_out);
+    decompress(file_out, file_in);
 
     return 0;
 }
